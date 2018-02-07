612d94ed3353d251c536959cad04b1a0c13be2a9
marta83 <prettykiller777@gmail.com>
2018-02-07 18:07:04 +0100

    Test create usecase

---
+++ spec/use_cases/create_use_case_spec.rb
@@ -0,0 +1,84 @@
+RSpec.describe 'Create use case' do
+  subject { CreateUseCase.call(attributes, callback, repository) }
+
+  let(:callback) { lambda do |entity| return entity end}
+
+  describe 'Create a zombie' do
+    let(:repository){ZombieRepository.new}
+
+    context "Valid attributes" do
+      let(:attributes) { { name: 'Zombie name', turn_date: DateTime.now - 1.week  } }
+
+      it 'should call without errors' do
+        expect { subject }.to_not raise_error
+      end
+
+      it 'should return zombie' do
+        expect( subject.name ).to eq("Zombie name")
+      end
+    end
+
+    context "Invalid attributes" do
+      let(:attributes) { { name: 'Zombie name'} }
+
+      it 'should call without errors' do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+
+  describe 'Create a Armor' do
+    let(:repository){ArmorRepository.new}
+
+    context "Valid attributes" do
+      let(:attributes) { { name: 'Armor name',
+                           price: 50,
+                           defense_points: 50,
+                           durability: 50} }
+
+      it 'should call without errors' do
+        expect { subject }.to_not raise_error
+      end
+
+      it 'should return armor' do
+        expect( subject.name ).to eq("Armor name")
+      end
+    end
+
+    context "Invalid attributes" do
+      let(:attributes) { { name: 'armor name'} }
+
+      it 'should call without errors' do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+
+  describe 'Create a Weapon' do
+    let(:repository){WeaponRepository.new}
+
+    context "Valid attributes" do
+      let(:attributes) { { name: 'Weapon name',
+                           price: 50,
+                           attack_points: 50,
+                           durability: 50} }
+
+      it 'should call without errors' do
+        expect { subject }.to_not raise_error
+      end
+
+      it 'should return weapon' do
+        expect( subject.name ).to eq("Weapon name")
+      end
+    end
+
+    context "Invalid attributes" do
+      let(:attributes) { { name: 'Weapon name'} }
+
+      it 'should call without errors' do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+
+end
