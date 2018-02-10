38b727c697c03e059f09f12b01310c32d0e05d9a
marta83 <prettykiller777@gmail.com>
2018-02-10 11:04:53 +0100

    Weapon can be updated

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,4 +1,5 @@
 class WeaponsController < ApplicationController
+  before_action :set_weapon, only: [:update, :destroy]
 
   def create
     success = lambda do |weapon|
@@ -12,11 +13,37 @@ class WeaponsController < ApplicationController
     CreateUseCase.call(weapon_params, repo, {success: success, failure: error})
   end
 
+  def update
+    success = lambda do |weapon|
+      json_response(weapon, :ok)
+    end
+
+    error = lambda do |error|
+      json_response(error, :unprocessable_entity)
+    end
+
+    UpdateUseCase.call(@weapon, weapon_params_update, repo, {success: success, failure: error})
+  end
+
+  private
+
+  def weapon_params_update
+    params.permit(:id, :name, :price, :attack_points, :durability)
+  end
+
   def weapon_params
     params.permit(:name, :price, :attack_points, :durability)
   end
 
   def repo
     @repo ||= Repository.for(:weapon)
+  end
+
+  def set_weapon
+    @weapon = repo.find(params[:id])
+  end
+
+  rescue_from Repository::WeaponNotFound do
+      json_response("Weapon not found", :not_found)
   end
 end
--- spec/requests/weapon_spec.rb
+++ spec/requests/weapon_spec.rb
@@ -50,4 +50,54 @@ RSpec.describe 'Weapons API', type: :request do
     end
   end
 
+  describe 'PUT /weapons' do
+
+    context 'when the request is valid' do
+      before(:all) {
+        weapon = create(:weapon)
+        valid_attributes = {  id: weapon.id, name: 'Weapon name changed'}
+        put weapon_path(valid_attributes)
+      }
+
+      it 'update weapon name' do
+        expect(json['name']).to eq('Weapon name changed')
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before(:all) {
+        attributes = {id: 1, name: 'Weapon name changed'}
+        put weapon_path(attributes)
+      }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body).to eq("Weapon not found")
+      end
+    end
+
+    context 'when the request params ar not invalid' do
+      before(:all) {
+        weapon = create(:weapon)
+        attributes = {id: weapon.id, name: 'Weapon name changed', attack_points: "asdasd"}
+        put weapon_path(attributes)
+      }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body).to eq("[\"Attack points is not a number\"]")
+      end
+    end
+
+  end
 end
